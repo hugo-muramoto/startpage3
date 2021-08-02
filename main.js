af7600257a25744ed30f0d12be4bbf1ff6c4b430
hugo-muramoto <80429220+hugo-muramoto@users.noreply.github.com>
2021-08-01 21:47:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -111,15 +111,15 @@ document.getElementById('relogio').innerHTML = relogio();
 
 const searchEngines = {
   Google: 'https://www.google.com.br/search?q=',
-  DuckDuckGo: "https://duckduckgo.com/?q=",
+  Duckduckgo: "https://duckduckgo.com/?q=",
   Brave: "https://search.brave.com/search?q=",
 }
 
 if (!Object.keys(searchEngines).includes(searchEngine)) {
-    searchEngine = "Brave"
+    searchEngine = "Duckduckgo"
 }
 
-var searchEngine = 'Brave';
+var searchEngine = 'Duckduckgo';
 
 var searchUrl = searchEngines[searchEngine];
 
